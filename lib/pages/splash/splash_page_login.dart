8a234ca9ed040a6566ecd1acc11ae00bc5900d55
ilCatta <andreaagostino.cataldo@gmail.com>
2023-07-19 23:10:59 +0200

    Com

--- lib/pages/splash/splash_page_login.dart
+++ lib/pages/splash/splash_page_login.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class SlashPageLogin extends StatelessWidget {
@@ -6,6 +7,83 @@ class SlashPageLogin extends StatelessWidget {
     return SizedBox(
       width: double.infinity,
       height: double.infinity,
+      child: Column(children: [
+        _titolo(),
+        _descrizione(),
+        SizedBox(height: 30),
+        _containerLogin(
+          text: "Accedi con Google",
+          bgColor: Colors.black,
+        ),
+        SizedBox(height: 20),
+        _containerLogin(
+          text: "Accedi con Apple",
+          bgColor: Colors.white,
+        ),
+        SizedBox(height: 30),
+        Text(
+          "Continua senza effettuare il login",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 13,
+          ),
+        )
+      ]),
+    );
+  }
+
+  Widget _titolo() => Padding(
+        padding: EdgeInsets.fromLTRB(0, 90, 0, 0),
+        child: Center(
+          child: Text(
+            "Login",
+            style: TextStyle(
+              fontSize: 33,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
+
+  Widget _descrizione() => Padding(
+        padding: EdgeInsets.fromLTRB(20, 28, 20, 0),
+        child: Center(
+          child: AutoSizeText(
+            "Ora che sai delle nsotre fantastiche funzioni, crea il tuo account per salvare i tuoi dati e averli sincronizzati su tutti i tuoi dispositivi!",
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 4,
+            minFontSize: 14,
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              letterSpacing: 1,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
+
+  Widget _containerLogin({
+    required String text,
+    required Color bgColor,
+  }) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+      height: 40,
+      width: double.infinity,
+      decoration: BoxDecoration(color: bgColor, borderRadius: BorderRadius.circular(16)),
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 16,
+            color: bgColor == Colors.black ? Colors.white : Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/pages/splash/splash_page_toggle_color.dart
+++ lib/pages/splash/splash_page_toggle_color.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nanea_demo/theme/theme.dart';
@@ -26,22 +27,31 @@ class _SlashPageToggleColorState extends State<SlashPageToggleColor> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: double.infinity,
-          height: (MediaQuery.of(context).size.height) / 2,
-          color: context.isLight ? Colors.white : Colors.black,
-          child: Center(
-            child: Image.asset(
-              context.isLight ? "assets/images/card-1.png" : "assets/images/card-2.png",
-              height: 320,
-              width: 320,
+    return GestureDetector(
+      onTap: widget.onTap,
+      child: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: (MediaQuery.of(context).size.height) / 2,
+              color: context.isLight ? Colors.white : Colors.black,
+              child: Center(
+                child: Image.asset(
+                  context.isLight ? "assets/images/card-1.png" : "assets/images/card-2.png",
+                  height: 320,
+                  width: 320,
+                ),
+              ),
             ),
-          ),
+            _toggleThemeColor(),
+            _titolo(),
+            _descrizione(),
+          ],
         ),
-        _toggleThemeColor(),
-      ],
+      ),
     );
   }
 
@@ -82,6 +92,40 @@ class _SlashPageToggleColorState extends State<SlashPageToggleColor> {
               ),
             ),
           ],
+        ),
+      );
+
+  Widget _titolo() => Padding(
+        padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+        child: Center(
+          child: Text(
+            "Vista",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
+
+  Widget _descrizione() => Padding(
+        padding: EdgeInsets.fromLTRB(20, 28, 20, 0),
+        child: Center(
+          child: AutoSizeText(
+            "Puoi fare il tuo ordine in modalità luce o modalità scura, come preferisci!",
+            textAlign: TextAlign.center,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 4,
+            minFontSize: 14,
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w500,
+              letterSpacing: 1,
+              color: Colors.white,
+            ),
+          ),
         ),
       );
 }
